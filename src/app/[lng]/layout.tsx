503de28f3f51f1669edc7d7a872b04d2b93a19d2
MohammedYoussefSoliman <m.youssef.soliman@gmail.com>
2023-08-17 16:57:35 +0300

    added fontsetup fixed presist issue

--- src/app/[lng]/layout.tsx
+++ src/app/[lng]/layout.tsx
@@ -1,13 +1,18 @@
 import "../globals.css";
 import type { Metadata } from "next";
 import { dir } from "i18next";
-import { Inter } from "next/font/google";
+import { Tajawal, Nunito } from "next/font/google";
 import type { PageProps } from "@/common.types";
 import AppProviders from "./AppProviders";
+import PersistStoreProvider from "./PersistStoreProvider";
 import AppEffects from "./AppEffects";
 import Header from "@components/Header";
 
-const inter = Inter({ subsets: ["latin"] });
+const nunito = Nunito({ subsets: ["latin"] });
+const tajawal = Tajawal({
+  subsets: ["arabic"],
+  weight: ["200", "300", "400", "500", "700", "800", "900"],
+});
 
 export const metadata: Metadata = {
   title: "Mazaady",
@@ -18,11 +23,13 @@ export default function RootLayout({ children, params: { lng } }: PageProps) {
   return (
     <AppProviders>
       <html lang={lng} dir={dir(lng)}>
-        <AppEffects />
-        <body className={inter.className}>
-          <Header />
-          {children}
-          <footer>footer</footer>
+        <body className={lng === "ar" ? tajawal.className : nunito.className}>
+          <PersistStoreProvider>
+            <AppEffects />
+            <Header />
+            {children}
+            <footer>footer</footer>
+          </PersistStoreProvider>
         </body>
       </html>
     </AppProviders>
