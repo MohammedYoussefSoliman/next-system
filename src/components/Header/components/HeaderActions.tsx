93788eee5548f4f24e3619847e7e026557fa91bb
MohammedYoussefSoliman <m.youssef.soliman@gmail.com>
2023-09-21 18:34:04 +0300

    added header action

---
+++ src/components/Header/components/HeaderActions.tsx
@@ -0,0 +1,30 @@
+"use client";
+import React from "react";
+import Divider from "@components/Divider";
+import { useAuth } from "@/hooks";
+import { Button, IconButton, LinkButton } from "@components/Button";
+import Link from "@components/Link";
+
+export default function HeaderAction() {
+  const { loggedIn } = useAuth();
+
+  return (
+    <div className="flex gap-8 py-2 items-center h-full">
+      {!loggedIn ? (
+        <div className="flex gap-4 items-center h-full">
+          <Link label="register" to="/register" />
+          <LinkButton to="/login">logIn</LinkButton>
+        </div>
+      ) : (
+        <Button>logOut</Button>
+      )}
+      <IconButton
+        icon="search"
+        variant="transparent"
+        iconColor="inherit"
+        className="enabled:hover:bg-slate-100"
+      />
+      <Divider type="vertical" className="bg-amber-200" />
+    </div>
+  );
+}
